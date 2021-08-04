01b1ddf5d460782ec3a7ad4df4a7678ea9659085
datlv <datlv@runsystem.net>
2021-08-04 17:58:37 +0700

    bai2

---
+++ script2.js
@@ -0,0 +1,44 @@
+function myFunction() {
+	const x = document.getElementById("selectdl");
+	let i = x.selectedIndex;
+  	var numb = x.options[i].value;
+  	check_number(numb);
+}
+function check_number(numb){
+	const li = document.getElementsByTagName("p");
+	const leng = li.length;
+	let i;
+ 	if(isNaN(numb) == false)
+ 	{	
+	 	for (i = 0; i < leng; i++) {
+	 		li[i].style.display="none";
+	   	 	li[numb-1].style.display = "block";
+	  	}	
+  	}
+  	if(numb == "chan")
+  	{	
+  		for (i = 0; i < leng; i++) {
+  			li[i].style.display ="none";
+	 		if(i % 2 != 0)
+	 		{
+		   	 	li[i].style.display = "block";
+	   	 	}
+	  	}
+  	}
+  	if(numb == "le")
+  	{	
+  		for (i = 0; i < li.length; i++) {
+  			li[i].style.display ="none";
+	 		if(i == 0 || i % 2 == 0)
+	 		{
+		   	 	li[i].style.display = "block";
+	   	 	}
+	  	}
+  	}
+  	if(numb == "reset")
+  	{
+  		for (i = 0; i < li.length; i++) {
+  			li[i].style.display ="none";	
+	  	}
+  	}
+}
