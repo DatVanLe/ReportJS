34b0fbd80b42a4ea8b310b5bf23f4e9f7b3b7665
datlv <datlv@runsystem.net>
2021-08-06 16:23:46 +0700

    jqbai2 update

--- script2.js
+++ script2.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-	$("#selectdl").change(function(){ 
+	$("#selectdl").change(function() { 
 	  	let numB = $("#selectdl").val();
-	  	checknumBer(numB);
+	  	checkNumber(numB);
 	});
-	function checknumBer(numB) {
+	function checkNumber(numB) {
 		const li = $("p");
 		const leng = li.length;
 		let i;
-	 	if (isNaN(numB) == false) {	
+	 	if (!isNaN(numB)) {	
 		 	for (i = 0; i < leng; i++) {
 		 		li[i].style.display="none";
 		   	 	li[numB-1].style.display = "block";
@@ -16,8 +16,7 @@ $(document).ready(function() {
 	  	if (numB == "chan") {	
 	  		for (i = 0; i < leng; i++) {
 	  			li[i].style.display ="none";
-		 		if(i % 2 != 0)
-		 		{
+		 		if (i % 2 != 0) {
 			   	 	li[i].style.display = "block";
 		   	 	}
 		  	}
@@ -25,7 +24,7 @@ $(document).ready(function() {
 	  	if (numB == "le") {	
 	  		for (i = 0; i < li.length; i++) {
 	  			li[i].style.display = "none";
-		 		if(i == 0 || i % 2 == 0) {
+		 		if (i == 0 || i % 2 == 0) {
 			   	 	li[i].style.display = "block";
 		   	 	}
 		  	}
