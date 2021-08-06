88f24fcd7f85af5d33e1ccd3b7acd12bfed8d474
datlv <datlv@runsystem.net>
2021-08-06 10:56:52 +0700

    finish bai1 jquery

--- script.js
+++ script.js
@@ -1,170 +1,173 @@
-$(document).ready(function() {
-	$(document).keyup(function(event) { 
+$(document).ready( function() {
+	$(document).keyup( function(event) { 
 		const kbshift = 16;
 		const kbdel = 46;
-		if(event.keyCode == kbshift)
-		{
+		if (event.keyCode == kbshift) {
 			$('#addform').click();
 		}
-		if(event.keyCode == kbdel)
-		{
+		if (event.keyCode == kbdel) {
 			$('#resetform').click();
 		}
 	});
-	function inputPhone(input) {
-		if(input.val().length == 3)
-		{
-			input.val() = input.val() + "-";
+	$('#phone').keyup( function() {
+		if ($(this).val().length == 3) {
+			$(this).val( $(this).val() + "-");
 		}
-		if(input.val().length == 7)
-		{
-			input.value = input.val() + "-";
+		if ($(this).val().length == 7) {
+			$(this).val( $(this).val() + "-");
+		}
+	});
+	$('#birthday').keyup( function() { 
+		if($(this).val().length == 2) {
+			$(this).val( $(this).val() + "-" );
+		}
+		if($(this).val().length == 5) {
+			$(this).val( $(this).val() + "-" );
+		}
+	});
+	$('#name').keyup( function() { 
+	 	const fixName = $(this).val().split(" ");
+		for (var i = 0; i < fixName.length; i++) {
+			fixName[i] = fixName[i].charAt(0).toUpperCase() + fixName[i].slice(1).toLowerCase();
+		}
+	 	$(this).val(fixName.join(" "));
+	});
+
+	$("#files").change( function(e) {
+		const output = $('#img');
+		const reoutput = $('#reimg');
+		output.attr('src', URL.createObjectURL(e.target.files[0]));
+		reoutput.attr('src', URL.createObjectURL(e.target.files[0]));
+		$("#img").css('display', 'block');	    
+		$("#label").text("Change Avartar");		   	    		    	    
+	});	
+	function checkNull(fieldName, value) {
+		err_text = value.parent().find("span");
+		if(value.val() == "") {
+			err_text.text("Vui Lòng Nhập " + fieldName);
+			return false;
+		}
+		err_text.text("");
+		return true;
+	}
+	function checkLength(fieldName, value, min , max) {
+		if(value.val() != "") {
+			err_text = value.parent().find("span");
+			if (value.val().length < min || value.val().length > max) {
+				err_text.text('Vui lòng nhập ' + fieldName + ' Lớn hơn ' + min + ' Nhỏ hơn ' + max + ' ký tự');
+				return false;
+			}
+			err_text.text("");
+			return true;
 		}
 	}
-	// function upFunction(input) {
-	// 	// let input=document.getElementById("input");	
-	// 	input.value = input.value.replace(/\w\S*/gi, function(letter) {
-	//    		return letter.charAt(0).toUpperCase() + letter.slice(1).toLowerCase();
-	//   	});
-	// }
-	// function inputBd(input) {
-	// 	if(input.value.length == 2)
-	// 	{
-	// 		input.value = input.value + "-";
-	// 	}
-	// 	if(input.value.length == 5)
-	// 	{
-	// 		input.value = input.value + "-";
-	// 	}
-	// }
-	// function change(event) {
-	// 	const output = document.getElementById('img');
-	// 	const reoutput = document.getElementById('reimg');
-	// 	output.src = URL.createObjectURL(event.target.files[0]);
-	// 	reoutput.src = URL.createObjectURL(event.target.files[0]);
-	// 	document.getElementById("img").style.display = "block";	    
-	// 	document.getElementById("label").innerHTML = "Change Avartar";		   	    		    	    
-	// };
-	// function getValue(name) {
-	// 	return document.getElementById(name).value;
-	// }
-	// var err = [];
-	// function checkNull(name, value) {	
-	// 	if(value == "")
-	// 	{
-	// 		err.push('Vui lòng nhập ' + name);
-	// 	}
-	// }
-	// function checkLength(name, value, min , max) {
-	// 	if((value > 0 && value < min) || value > max)
-	// 	{
-	// 		err.push('Vui lòng nhập ' + name + ' lớn hơn ' + min + ' nhỏ hơn ' + max + ' ký tự');
-	// 	}
-	// }
-	// function checkRegx(name, value, pattern) {
-	// 	if(value.match(pattern))
-	// 	{
-	// 		err.push('Vui lòng kiểm tra ký tự đặc biệt + định dạng + qui ước của ' + name);
-	// 	}
-	// }
-	// function checkFormat(name, value, format) {
-	// 	if(value != "" && !value.match(format))
-	// 	{
-	// 		err.push('Vui lòng kiểm tra định dạng của ' + name);
-	// 	}
-	// }
-	// function checkFormatDate(name, value) {
-	// 	var d = new Date();
-	// 	var year = d.getFullYear();
-	// 	var lastDay = new Date(value.slice(6), value.slice(3,5), 0);
-	// 	var date = lastDay.getDate();
-	// 	if(value.slice(3,5) > 12 || value.slice(0,2) > date || value.slice(6) >= year)
-	// 	{	
-	// 		err.push('Xin vui lòng kiểm tra lại ngày tháng năm của ' + name);
-	// 	}
-	// }
-	// function checkImg(name, value) {
-	// 	if(value != "")
-	// 	{
-	// 		const Extension = value.substring(
-	//         value.lastIndexOf('.') + 1).toLowerCase();
-	// 		if(Extension == "png" || Extension == "bmp" || Extension == "jpg")
-	// 		{	
-	// 		}else{
-	// 			err.push("Vui lòng chọn ảnh có đuôi .png .jpg .bmp " + name);
-	// 		}
-	// 	}
-	// }
-	// function funValiDate() {
-	// 	event.preventDefault();
-	// 	err = [];
-	// 	//name
-	// 	const name = getValue('name');
-	// 	const length_name = getValue('name').length;
-	// 	const pattern_name = /[0-9]|[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
-	// 	checkNull('Tên',name);	
-	// 	checkLength('Tên', length_name, 8, 50);
-	// 	checkRegx('Tên', name, pattern_name);
-	// 	//email
-	// 	const email = getValue('email');
-	// 	const length_email = getValue('email').length;
-	// 	const format_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	// 	checkNull('Email', email);
-	// 	checkLength('Email', length_email, 8, 100);
-	// 	checkFormat('Email', email, format_email);
-	// 	//phone
-	// 	const phone = getValue('phone');
-	// 	const pattern_phone = /[a-zA-Z]|[`!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?~]/g;
-	// 	const format_phone = /^0[0-9]{2}-[0-9]{3}-[0-9]{4}/;
-	// 	checkNull('Phone', phone);
-	// 	checkRegx('Phone', phone, pattern_phone);
-	// 	checkFormat('Phone', phone, format_phone);
-	// 	//birthday
-	// 	const birthday = getValue('birthday');
-	// 	const format_birthday = /^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
-	// 	checkNull('BirthDay', birthday);
-	// 	checkFormat('BirthDay', birthday, format_birthday);
-	// 	checkFormatDate('BirthDay', birthday);
-	// 	//password
-	// 	const password = getValue('password');
-	// 	const length_password = getValue('password').length;
-	// 	const format_password = /^[A-Z]{1}[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
-	// 	checkNull('Password', password);
-	// 	checkLength('Password', length_password, 8, 30);
-	// 	checkFormat('Password', password, format_password);
-	// 	//repassword
-	// 	const repassword = getValue('repassword');
-	// 	checkNull('Repassword', repassword);
-	// 	if(repassword != "" && repassword != password)
-	// 	{
-	// 		err.push('Chưa khớp với password');
-	// 	}
-	// 	//img
-	// 	const img = getValue('files');
-	// 	checkNull('IMG', img);
-	// 	checkImg('Avartar', img);
-	// 	//no err
-	// 	if(err != "")
-	// 	{	
-	// 		let txt = "<ul>";
-	// 		for(let i in err)
-	// 		{
-	// 			txt += "<li>" + err[i] + " " + "</li>" + "<br>";
-	// 		}
-	// 		document.getElementById("err").innerHTML = txt;
-	// 		txt += "</ul>";
-	// 		event.preventDefault();
-	// 	}
-	// 	if(err == "")
-	// 	{
-	// 		document.getElementById("fullname").innerHTML = name;
-	// 		document.getElementById("fullemail").innerHTML = email;
-	// 		document.getElementById("fullphone").innerHTML = phone;
-	// 		document.getElementById("fullbirthday").innerHTML = birthday;
-	// 		document.getElementById("reimg").style.display = "block";
-	// 		document.getElementById("avatar").style.display = "none";
-	// 		document.getElementById("err").innerHTML = "";
-	// 		event.preventDefault();
-	// 	}	
-	// }
+	function checkRegx(fieldName, value, pattern) {
+		if(value.val() != "") {
+			err_text = value.parent().find("span");
+			if(value.val().match(pattern)) {
+				err_text.text('Vui lòng kiểm tra ký tự đặc biệt + định dạng + qui ước của ' + fieldName);
+				return false;
+			}
+			err_text.text("");
+			return true;
+		}
+	}
+	function checkFormat(fieldName, value, format) {
+		if(value.val() != "") {
+			err_text = value.parent().find("span");
+			if(!value.val().match(format)) {
+				err_text.text('Vui lòng kiểm tra định dạng của ' + fieldName);
+				return false;
+			}
+			err_text.text("");
+			return true;
+		}
+	}
+
+	function checkFormatDate(filedName, value) {
+		if(value.val() != "") {
+			err_text = value.parent().find("span");
+			var d = new Date();
+			var year = d.getFullYear();
+			var lastDay = new Date(value.val().slice(6), value.val().slice(3,5), 0);
+			var date = lastDay.getDate();
+			if(value.val().slice(3,5) > 12 || value.val().slice(0,2) > date || value.val().slice(6) >= year) {	
+				err_text.text('Xin vui lòng kiểm tra lại ngày tháng năm của ' + filedName);
+				return false;
+			}
+			return true;
+		}
+	}
+	function checkImg(fieldName, value) {
+		if(value.val() != "") {	
+			err_text = value.parent().find("span");
+			const Extension = value.val().substring(
+	        value.val().lastIndexOf('.') + 1).toLowerCase();
+			if(Extension == "png" || Extension == "bmp" || Extension == "jpg") {	
+			} else {
+				err_text.text("Vui lòng chọn ảnh có đuôi .png .jpg .bmp " + fieldName);
+				return false;
+			}
+			return true;
+		}
+	}
+	$("#addform").click( function() {
+		let flag = true;
+		event.preventDefault();
+	// 	//const variable
+		const name = $("#name");
+		const email = $("#email");
+		const phone = $("#phone");
+		const birthday = $("#birthday");
+		const password = $("#password");
+		const repassword = $("#repassword");
+		const image = $("#files");
+		//check null 
+		flag = checkNull("Tên", name);
+		flag = checkNull("Email", email);
+		flag = checkNull('Phone', phone);
+		flag = checkNull('Birthday', birthday);
+		flag = checkNull('Password', password);
+		flag = checkNull('Repassword', repassword);
+		flag = checkNull('Image', image);
+		// check length
+		flag = checkLength('Họ và Tên', name, 8, 50 );
+		flag = checkLength('Email', email, 1, 100);
+		flag = checkLength('Password', password, 8, 30);
+		//pattern
+		const patternName = /[0-9]|[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;	
+		const patternPhone = /[a-zA-Z]|[`!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?~]/g;
+		const formatEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		const formatPhone = /^[0][0-9]{2}-[0-9]{3}-[0-9]{4}/;
+		const formatPassword = /^[A-Z]{1}[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
+		const formatBirthday = /^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
+		// check Regex
+		flag = checkRegx('Tên', name, patternName);
+		flag = checkRegx('Phone', phone, patternPhone);
+		
+		//checkformat
+		flag = checkFormat('Email', email, formatEmail);
+		flag = checkFormat('Phone', phone, formatPhone);
+		flag = checkFormat('Password', password, formatPassword);
+		flag = checkFormat('BirthDay', birthday, formatBirthday);
+		flag = checkFormatDate('BirthDay', birthday);
+		flag = checkImg('Images', image);	
+		//check repassword
+		if(repassword.val() != "" && repassword.val() != password.val())
+		{
+			err_text = repassword.parent().find("span");
+			err_text.text('Chưa khớp với password');
+			return flag = false;
+		}else{return flag = true;}
+		//result
+		if(flag) {
+			$("#fullname").text(name.val());
+			$("#fullemail").text(email.val());
+			$("#fullphone").text(phone.val());
+			$("#fullbirthday").text(birthday.val());
+			$("#reimg").css('display', 'block');
+			$("#avatar").css('display', 'none');
+			return true;	
+		} else { return false;}
+	});
 });
